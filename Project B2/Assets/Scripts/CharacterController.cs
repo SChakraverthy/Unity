f43f4f4f9fb9408a4c3735fd37d8c56e2201e755
Swapna Chakraverthy <sac340@scarletmail.rutgers.edu>
2017-10-18 16:32:26 -0400

    Changes to idle animation, character controller

--- Project B2/Assets/Scripts/CharacterController.cs
+++ Project B2/Assets/Scripts/CharacterController.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour {
+public class CharacterController : MonoBehaviour
+{
 
     #region Enum
 
-    public enum KEYBOARD_INPUT: int
+    public enum KEYBOARD_INPUT : int
     {
 
         FORWARDS = KeyCode.W,
@@ -24,27 +25,32 @@ public class CharacterController : MonoBehaviour {
     #region Vars
 
     Animator animator;
+    bool run;
 
     #endregion
 
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
 
         animator = GetComponent<Animator>();
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         var x = Input.GetAxis("Horizontal");
         var y = Input.GetAxis("Vertical");
         var r = Input.GetAxis("Rotate");
 
+        Debug.Log(message: "Horizontal: " + x + "Vertical" + y);
 
-        bool run = false;
 
-        if (Input.GetKeyDown("left shift"))
+        run = false;
+
+        if (Input.GetKey(KeyCode.LeftShift))
         {
             run = true;
         }
@@ -60,20 +66,34 @@ public class CharacterController : MonoBehaviour {
 
     void Move(float x, float y, float r)
     {
-        animator.SetFloat("velx", x);
-        animator.SetFloat("vely", y);
+
+        if (run)
+        {
+            animator.SetFloat("velx", x);
+            animator.SetFloat("vely", y);
+
+        }
+        else
+        {
+            animator.SetFloat("velx", x * 0.5f);
+            animator.SetFloat("vely", y * 0.5f);
+        }
+
         transform.Rotate(0, r, 0);
-        
+        transform.position += transform.forward * 10 * y * Time.deltaTime;
+        transform.position += transform.right * 10 * x * Time.deltaTime;
 
 
+
+
+        /*
         //if (run)
         //{
 
-        transform.position += transform.forward * 10 * y * Time.deltaTime;
-            transform.position += transform.right * 10 * x * Time.deltaTime;
+        
 
 
-        //    }
+        //    }*/
 
     }
 }
