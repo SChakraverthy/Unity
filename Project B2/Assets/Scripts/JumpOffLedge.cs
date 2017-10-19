38cc56adc78f6b4bc94159adf8a03e771137ec05
Randy <rem180@scarletmail.rutgers.edu>
2017-10-18 21:48:35 -0400

    Beginning work on jumping offmesh links

---
+++ Project B2/Assets/Scripts/JumpOffLedge.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpOffLedge : MonoBehaviour {
+
+    UnityEngine.AI.NavMeshAgent agent;
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<animator>();
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+	// Update is called once per frame
+	void Update () {
+		if (agent.isOnOffmeshLink)
+        {
+
+        }
+	}
+}
