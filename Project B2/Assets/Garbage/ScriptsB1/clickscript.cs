75b1233dd11de71564672a3e64aa86b21ad2fc7c
Randy Mester <rem180@scarletmail.rutgers.edu>
2017-10-19 20:18:07 -0400

    Part 3 implemented but wonky

---
+++ Project B2/Assets/Garbage/ScriptsB1/clickscript.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickscript : MonoBehaviour
+{
+
+    UnityEngine.AI.NavMeshAgent agent;
+    UnityEngine.AI.NavMeshAgent tempAgent;
+
+    GameObject movObj;
+
+    bool active;
+    bool obstacleActive;
+
+    Queue<UnityEngine.AI.NavMeshAgent> agentQueue;
+
+    // Use this for initialization
+    void Start()
+    {
+        active = false;
+        obstacleActive = false;
+        GameObject movObj = null;
+        agentQueue = new Queue<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0) && !obstacleActive)
+        {
+            RaycastHit hit;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+
+                if (hit.collider.tag == "agent" && hit.collider.gameObject.GetComponent<Renderer>().material.color != Color.blue)
+                {
+                    agent = hit.collider.gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+                    agentQueue.Enqueue(agent);
+                    agent.GetComponent<Renderer>().material.color = Color.blue;
+                    active = true;
+                }
+                else if (hit.collider.tag == "agent" && hit.collider.gameObject.GetComponent<Renderer>().material.color == Color.blue)
+                {
+
+                    agent = hit.collider.gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+                    agent.GetComponent<Renderer>().material.color = Color.white;
+
+                    tempAgent = agentQueue.Dequeue();
+
+                    while (tempAgent != agent)
+                    {
+                        agentQueue.Enqueue(tempAgent);
+                        tempAgent = agentQueue.Dequeue();
+                    }
+
+                    if (agentQueue.Count == 0)
+                    {
+                        active = false;
+                    }
+                }
+                else if (Physics.Raycast(ray, out hit) && active && hit.collider.tag != "agent")
+                {
+                    while (agentQueue.Count > 0)
+                    {
+                        agent = agentQueue.Dequeue();
+                        agent.destination = hit.point;
+                        agent.GetComponent<Renderer>().material.color = Color.white;
+                    }
+                    active = false;
+                }
+                else if (Physics.Raycast(ray, out hit) && hit.collider.tag == "obstacle")
+                {
+                    movObj = hit.collider.gameObject;
+                    hit.collider.gameObject.GetComponent<Renderer>().material.color = Color.red;
+                    obstacleActive = true;
+                }
+            }
+        }
+        else if (obstacleActive && Input.GetKeyDown("left"))
+        {
+            movObj.transform.Translate(-1.0f, 0.0f, 0.0f, Space.Self);
+        }
+        else if (obstacleActive && Input.GetKeyDown("right"))
+        {
+            movObj.transform.Translate(1.0f, 0.0f, 0.0f, Space.Self);
+        }
+        else if (obstacleActive && Input.GetKeyDown("return"))
+        {
+            movObj.GetComponent<Renderer>().material.color = Color.white;
+            obstacleActive = false;
+        }
+
+    }
+}
---
+++ Project B2/Assets/Scripts/CameraControllerPart3.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControllerPart3 : MonoBehaviour
+{
+
+    #region Enum
+
+    public enum KEYBOARD_INPUT : int
+    {
+
+        C_FORWARD = KeyCode.S,
+        C_BACKWARDS = KeyCode.W,
+        C_LEFT = KeyCode.D,
+        C_RIGHT = KeyCode.A,
+        SPEED_MOD = KeyCode.LeftShift,
+
+    }
+
+    #endregion
+
+    #region Vars
+
+    private Transform Camera_Transform;
+    private float speed_mod;
+    private float zoom_speed;
+
+    #endregion
+
+    #region Controller
+    // Use this for initialization
+    void Start()
+    {
+
+        Camera_Transform = Camera.main.transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Camera XZ Movement
+        speed_mod = Input.GetKey((KeyCode)KEYBOARD_INPUT.SPEED_MOD) ? 7f : 1f;
+        zoom_speed = 15f;
+
+        foreach (KEYBOARD_INPUT val in Enum.GetValues(typeof(KEYBOARD_INPUT)))
+        {
+
+            if (Input.GetKey((KeyCode)val))
+            {
+
+                switch (val)
+                {
+
+                    case KEYBOARD_INPUT.C_FORWARD:
+
+                        Camera_Transform.Translate(Vector3.forward * speed_mod * Time.deltaTime, Space.World);
+                        break;
+
+                    case KEYBOARD_INPUT.C_BACKWARDS:
+
+                        Camera_Transform.Translate(Vector3.back * speed_mod * Time.deltaTime, Space.World);
+                        break;
+
+                    case KEYBOARD_INPUT.C_RIGHT:
+
+                        Camera_Transform.Translate(Vector3.right * speed_mod * Time.deltaTime, Space.World);
+                        break;
+
+                    case KEYBOARD_INPUT.C_LEFT:
+
+                        Camera_Transform.Translate(Vector3.left * speed_mod * Time.deltaTime, Space.World);
+                        break;
+
+                }
+            }
+        }
+
+        // Camera Y Movement
+        float zoomDelta = Input.GetAxis("Mouse ScrollWheel");
+        if (zoomDelta > 0f)
+        {
+            // Zoom In
+            Camera_Transform.Translate(Vector3.down * zoom_speed * Time.deltaTime, Space.World);
+        }
+
+        if (zoomDelta < 0f)
+        {
+            //Zoom Out
+            Camera_Transform.Translate(Vector3.up * zoom_speed * Time.deltaTime, Space.World);
+        }
+    }
+    #endregion
+
+}
