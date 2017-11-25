d054e0a725ef6795a9420a7893f2b559aa16776d
Swapna Chakraverthy <sac340@scarletmail.rutgers.edu>
2017-11-24 20:03:17 -0500

    Added KADAPT folders to B4

---
+++ Project B4/Project B4/Assets/Scripts/Tree1.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using TreeSharpPlus;
+
+public class Tree1 : MonoBehaviour
+{
+    public GameObject waiter;
+    public GameObject chef;
+
+    private BehaviorAgent behaviorAgent;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        behaviorAgent = new BehaviorAgent(this.BuildTreeRoot());
+        BehaviorManager.Instance.Register(behaviorAgent);
+        behaviorAgent.StartBehavior();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    protected Node ST_Converse(GameObject waiter, GameObject chef)
+    {
+
+        Vector3 chefPos = this.chef.transform.position;
+        Vector3 chefPosHead = chefPos + new Vector3(0, 3.0f, 0);
+        Vector3 waiterPos = this.waiter.transform.position;
+
+
+        return new Sequence(this.waiter.GetComponent<BehaviorMecanim>().Node_GoToUpToRadius(chefPos, 3.0f), 
+            new LeafWait(500), this.chef.GetComponent<BehaviorMecanim>().Node_OrientTowards(waiterPos));
+
+    }
+
+    protected Node BuildTreeRoot()
+    {
+        Node conversation = new DecoratorLoop(new Sequence(this.ST_Converse(this.waiter, this.chef)));
+
+        return conversation;
+    }
+
+}
