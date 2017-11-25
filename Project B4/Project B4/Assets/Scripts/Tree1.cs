92b43e7e411e01a02519def9e17e25025300173c
Swapna Chakraverthy <sac340@scarletmail.rutgers.edu>
2017-11-24 21:59:16 -0500

    Updated Tree1 script

--- Project B4/Project B4/Assets/Scripts/Tree1.cs
+++ Project B4/Project B4/Assets/Scripts/Tree1.cs
@@ -7,6 +7,7 @@ public class Tree1 : MonoBehaviour
 {
     public GameObject waiter;
     public GameObject chef;
+    public GameObject manager;
 
     private BehaviorAgent behaviorAgent;
 
@@ -30,17 +31,18 @@ public class Tree1 : MonoBehaviour
 
         Vector3 chefPos = this.chef.transform.position;
         Vector3 waiterPos = this.waiter.transform.position;
+        Vector3 managerPos = this.manager.transform.position;
 
-
-        return new Sequence(this.waiter.GetComponent<BehaviorMecanim>().Node_GoToUpToRadius(chefPos, 10.0f), 
+        return new Sequence(this.waiter.GetComponent<BehaviorMecanim>().Node_GoToUpToRadius(chefPos, 5.0f), 
             this.waiter.GetComponent<BehaviorMecanim>().Node_OrientTowards(chefPos), 
-            new LeafWait(500), this.chef.GetComponent<BehaviorMecanim>().Node_OrientTowards(waiterPos));
+            new LeafWait(500), this.chef.GetComponent<BehaviorMecanim>().Node_OrientTowards(waiterPos), new LeafWait(5000), 
+            this.waiter.GetComponent<BehaviorMecanim>().Node_GoToUpToRadius(managerPos, 4.0f), this.waiter.GetComponent<BehaviorMecanim>().Node_OrientTowards(managerPos));
 
     }
 
     protected Node BuildTreeRoot()
     {
-        Node conversation = new DecoratorLoop(new Sequence(this.ST_Converse(this.waiter, this.chef)));
+        Node conversation = new Sequence(this.ST_Converse(this.waiter, this.chef));
 
         return conversation;
     }
