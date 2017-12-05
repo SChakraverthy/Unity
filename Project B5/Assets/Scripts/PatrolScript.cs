3a933aedbd003aded2ec38f3a8552d00c7a22c11
Swapna Chakraverthy <sac340@scarletmail.rutgers.edu>
2017-12-04 21:03:44 -0500

    Added PatrolScript

---
+++ Project B5/Assets/Scripts/PatrolScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PatrolScript : MonoBehaviour {
+
+    #region Vars
+
+    private Animator animator;
+
+    public Transform[] controlPoints;
+    private Rigidbody rb;
+    private int indexTarget;
+    private NavMeshAgent agent;
+
+    #endregion
+
+    #region PatrolController
+    // Use this for initialization
+    void Start () {
+        animator = this.GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+        GoToNextPoint();
+        
+	}
+
+    void GoToNextPoint()
+    {
+        animator.SetBool("Move", true);
+
+        if (controlPoints.Length == 0)
+        {
+            return;
+        }
+
+        agent.destination = controlPoints[indexTarget].position;
+        indexTarget = (indexTarget + 1) % controlPoints.Length;
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            GoToNextPoint();
+        }
+            
+    }
+
+    
+
+    #endregion
+}
