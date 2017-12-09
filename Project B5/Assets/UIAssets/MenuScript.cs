0b51d801c807ce2a25dbd1863ea6c2b1786c5229
Swapna Chakraverthy <sac340@scarletmail.rutgers.edu>
2017-12-09 10:57:28 -0500

    Added main menu UI with temporary background.

---
+++ Project B5/Assets/UIAssets/MenuScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+    public GameObject InstructionsPanel;
+    public GameObject LoadLevelPanel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadNewScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ShowInstructions()
+    {
+        InstructionsPanel.SetActive(true);
+    }
+
+    public void HideInstructions()
+    {
+        InstructionsPanel.SetActive(false);
+    }
+
+    public void ShowLoadLevel()
+    {
+        LoadLevelPanel.SetActive(true);
+    }
+
+    public void HideLoadLevel()
+    {
+        LoadLevelPanel.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
