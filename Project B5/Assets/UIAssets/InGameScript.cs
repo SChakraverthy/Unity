da6676e93fb34ad74dd6927f48995a4ed3e641fa
Swapna Chakraverthy <sac340@scarletmail.rutgers.edu>
2017-12-09 23:19:19 -0500

    Added In-Game panels to hub and future zones

---
+++ Project B5/Assets/UIAssets/InGameScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameScript : MonoBehaviour {
+
+    public GameObject InGamePanel;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+            if (InGamePanel.activeInHierarchy)
+            {
+                InGamePanel.SetActive(false);
+            }
+            else
+            {
+                InGamePanel.SetActive(true);
+            }
+        }
+
+    }
+
+    public void LoadNewLevel(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
